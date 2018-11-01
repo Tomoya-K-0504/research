dc49142e96e822a8b8bf921d48f0b211612435e1
Tomoya Koike <makeffort134@gmail.com>
2018-11-01 20:09:07 +0900

    can scrape weather table info on condition of postal code, year, month and day

--- weather/scrape.py
+++ weather/scrape.py
@@ -1,4 +1,4 @@
-import os, time, re
+import os, time, re, math
 import requests
 import pandas as pd
 import sys
@@ -10,21 +10,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def zip2geo(postcode):
-    """
-    郵便番号を引数として、緯度と経度を返す.参考サイト→https://renztech.wordpress.com/2018/02/28/%E9%83%B5%E4%BE%BF%E7%95%AA%E5%8F%B7%E3%82%92%E7%B7%AF%E5%BA%A6%E7%B5%8C%E5%BA%A6%E3%81%AB%E5%A4%89%E6%8F%9B%E3%81%99%E3%82%8B/
-    :param postcode: 7桁の数字(str)で、ハイフン可
-    :return: 緯度と経度
-    """
-    # ハイフンを除く
-    postcode = postcode.replace("-", "")
-
-    zip2geo_df = pd.read_csv("./Zip2Geoc.csv")
-    (longitude, latitude) = zip2geo_df[zip2geo_df["Zip"] == int(postcode)][["Lon", "Lat"]].values[0]
-
-    return (longitude, latitude)
-
-
 def access_site(url):
     html = requests.get(url).content
     soup = BeautifulSoup(html, "lxml")
@@ -33,22 +18,34 @@ def access_site(url):
 
 
 class JmaScraper:
-    def __init__(self, year=2010, month=1, day=1):
+    def __init__(self, geo_data, year="", month="", day="", prec_no="", block_no=""):
+        self.postal = geo_data["postal"]
+        self.pref = geo_data["prefecture"]
+        self.city = geo_data['city']
+        self.town = geo_data['town']
+        self.longitude = float(geo_data["x"])
+        self.latitude = float(geo_data["y"])
         self.year = year
         self.month = month
         self.day = day
-        self.base_url = f"http://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year={self.year}&month={self.month}&day={self.day}&view=a2"
-
-        self.html = requests.get(self.base_url).content
-        self.soup = BeautifulSoup(self.html, "lxml")
-        self.pref_code_df = self._load_pref_code()
+        self.prec_no = prec_no
+        self.block_no = block_no
+        # self.html, self.soup = access_site(self.base_url)
+        self._fill_prec_no(self)
+        self.pref_select_page_url = f"http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture.php?prec_no={self.prec_no}"
 
     @staticmethod
-    def _read_table(self):
-        # htmlソースからレース結果のテーブルを探してくる
-        table = self.soup.find("table", class_="data2_s")
+    def _update_base_url(self):
+        self.base_url = f"http://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no={self.prec_no}&block_no={self.block_no}&year={self.year}&month={self.month}&day={self.day}&view=a2"
 
-        df = pd.read_html(table.prettify(), header=0)
+    @staticmethod
+    def _fill_prec_no(self):
+        """
+        __init__で与えられた県名で、prec_noを検索して代入する
+        :return:
+        """
+        pref_code_df = self._load_pref_code()
+        self.prec_no = pref_code_df[pref_code_df["pref"] == self.pref]["code"].values[0]
 
     @staticmethod
     def _load_pref_code():
@@ -62,7 +59,7 @@ class JmaScraper:
         url = "http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00.php"
         html, soup = access_site(url)
         points = soup.find_all("area")
-        # TODO 北海道の地方名をどうするか
+        # TODO 北海道の地方名をどうするか. 未対応
 
         columns_dict = {"pref": "", "code": 0}
         pref_code_df = pd.DataFrame(columns=columns_dict.keys())
@@ -75,12 +72,75 @@ class JmaScraper:
 
         return pref_code_df
 
+    @staticmethod
+    def _scrape_table(self, soup):
+        # htmlソースからレース結果のテーブルを探してくる
+        table = soup.find("table", class_="data2_s")
+
+        df = pd.read_html(table.prettify(), header=0)[0]
+
+        return df
+
+    def search_nearest_block(self):
+        self._update_base_url(self)
+        html, soup = access_site(self.pref_select_page_url)
+        all_points = soup.find_all("area")
+
+        nearest_point = all_points[0]
+        nearest_distance = self._calc_distance(self, nearest_point)
+        for point in all_points:
+            if "onmouseover" not in point.attrs.keys():
+                continue
+            distance = self._calc_distance(self, point)
+            if distance < nearest_distance:
+                nearest_point = point
+                nearest_distance = distance
+
+        return nearest_point
+
+    @staticmethod
+    def _calc_distance(self, point):
+        try:
+            lat = float(point["onmouseover"].split(",")[4][1:-1]) + float(point["onmouseover"].split(",")[5][1:-1]) / 60
+            lon = float(point["onmouseover"].split(",")[6][1:-1]) + float(point["onmouseover"].split(",")[7][1:-1]) / 60
+        except KeyError as e:
+            print(point)
+            sys.exit(1)
+        return math.sqrt((lat - self.latitude) ** 2 + (lon - self.longitude) ** 2)
+
+    def scrape(self):
+        point = self.search_nearest_block()
+
+        self.block_no = point["href"].split("block_no=")[1].split("&")[0]
+        self._update_base_url(self)
+        html, soup = access_site(self.base_url)
+
+        df = self._scrape_table(self, soup)
+        return df
+
+
+def zip2geo(postal):
+    """
+
+    :param postal:
+    res['postal'], res['prefecture'], res['city'], res['town'], res['y'], res['x']
+    :return: 6570805, 兵庫県, 神戸市灘区, 青谷町一丁目, 34.712429, 135.214521
+    """
+    url = 'http://geoapi.heartrails.com/api/json'
+    payload = {'method': 'searchByPostal'}
+    payload['postal'] = '657-0805'
+
+    res = requests.get(url, params=payload).json()['response']['location'][0]
+    print('%s, %s, %s, %s, %s, %s\n' % (res['postal'], res['prefecture'], res['city'], res['town'], res['y'], res['x']))
+
+    return res
+
 
 if __name__ == "__main__":
 
-    # URLで年と月ごとの設定ができるので%sで指定した英数字を埋め込めるようにします。
     postcode = "657-0026"
-    jma_scraper = JmaScraper()
-    longitude, latitude = zip2geo(postcode)
+    get_data = zip2geo(postcode)
+    jma_scraper = JmaScraper(get_data, year=2018, month=10, day=10)
+    df = jma_scraper.scrape()
 
     pass
