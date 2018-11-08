6d359c9429484631f5d1c964475679be43b616a0
Tomoya Koike <makeffort134@gmail.com>
2018-11-08 19:21:06 +0900

    :update can make daily and hourly df, and merge them

--- weather/zip2weather.py
+++ weather/zip2weather.py
@@ -13,6 +13,7 @@ import const
 
 
 def access_site(url):
+    time.sleep(0.5)
     html = requests.get(url).content
     soup = BeautifulSoup(html, "lxml")
 
@@ -20,27 +21,21 @@ def access_site(url):
 
 
 class JmaScraper:
-    def __init__(self, geo_data, year="", month="", day="", prec_no="", block_no="", mode='daily'):
-        self.postal = geo_data["postal"]
-        self.pref = geo_data["prefecture"]
-        self.city = geo_data['city']
-        self.town = geo_data['town']
-        self.longitude = float(geo_data["x"])
-        self.latitude = float(geo_data["y"])
+    def __init__(self, pref, year="", month="", day="", prec_no="", block_no="", mode='daily'):
+        self.pref = pref
         self.year = year
         self.month = month
         self.day = day
-        self.prec_no = prec_no
+        self.prec_no = prec_no # 県ごとにつけられる番号
         self.block_no = block_no
         self.mode = "daily"
-        self.unknown = "s1"
         # self.html, self.soup = access_site(self.base_url)
         self._fill_prec_no(self)
         self.pref_select_page_url = f"http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture.php?prec_no={self.prec_no}"
 
     @staticmethod
     def _update_base_url(self, unknown="s1"):
-        self.base_url = f"http://www.data.jma.go.jp/obd/stats/etrn/view/{self.mode}_{unknown}.php?prec_no={self.prec_no}&block_no={self.block_no}&year={self.year}&month={self.month}&day={self.day}&view=p1"
+        self.base_url = f"http://www.data.jma.go.jp/obd/stats/etrn/view/{self.mode}_s1.php?prec_no={self.prec_no}&block_no={self.block_no}&year={self.year}&month={self.month}&day={self.day}&view=p1"
 
     @staticmethod
     def _fill_prec_no(self):
@@ -80,74 +75,75 @@ class JmaScraper:
     def _scrape_table(self, soup, class_name="data2_s"):
         # htmlソースからレース結果のテーブルを探してくる
         table = soup.find("table", class_=class_name)
-        if not table:
-            return pd.DataFrame()
 
         df = pd.read_html(table.prettify(), header=0)[0]
 
         return df
 
-    def search_nearest_block(self):
+    def search_observatory(self):
         self._update_base_url(self)
         html, soup = access_site(self.pref_select_page_url)
         all_points = soup.find_all("area")
+        big_observatories = []
 
-        nearest_point = all_points[0]
-        nearest_distance = self._calc_distance(self, nearest_point)
         for point in all_points:
             if "onmouseover" not in point.attrs.keys():
                 continue
-            distance = self._calc_distance(self, point)
-            if distance < nearest_distance:
-                nearest_point = point
-                nearest_distance = distance
 
-        return nearest_point
+            if point["onmouseover"].split(",")[13][1:2] == '1':
+                big_observatories.append(point)
+
+        return big_observatories
 
     @staticmethod
-    def _calc_distance(self, point):
-        try:
-            lat = float(point["onmouseover"].split(",")[4][1:-1]) + float(point["onmouseover"].split(",")[5][1:-1]) / 60
-            lon = float(point["onmouseover"].split(",")[6][1:-1]) + float(point["onmouseover"].split(",")[7][1:-1]) / 60
-        except KeyError as e:
-            print(point)
-            sys.exit(1)
-        return math.sqrt((lat - self.latitude) ** 2 + (lon - self.longitude) ** 2)
+    def _get_block_no(self, points):
+        return list(set([point["href"].split("block_no=")[1].split("&")[0] for point in points]))
 
-    def scrape(self, mode="daily"):
-        if mode != self.mode:
-            self.mode = mode
-
-        # 緯度経度に最も近い地点を取得
-        point = self.search_nearest_block()
-
-        self.block_no = point["href"].split("block_no=")[1].split("&")[0]
+    @staticmethod
+    def _extract_df_from_html(self):
         self._update_base_url(self)
         html, soup = access_site(self.base_url)
 
-        # スクレイプしたページからテーブルをdataframeとして抽出する
+        # スクレイプしたページから、テーブルをdataframeの形式で抽出する
         df = self._scrape_table(self, soup)
 
-        # アクセスしたページがない場合
-        if df.empty:
-            self.unknown = "a1"
-            self._update_base_url(self)
-            html, soup = access_site(self.base_url)
-            df = self._scrape_table(self, soup, class_name="data2_s")
-
-        df = self._arrange_df(self, df)
+        if self.mode == "daily":
+            df = df[3:]
+            df.columns = const.daily_columns_s1
+        else:
+            df = df[1:]
+            df.columns = const.hourly_columns_s1
 
         return df
 
-    @staticmethod
-    def _arrange_df(self, df):
+    def scrape(self, **kwargs):
 
-        if self.unknown == "a1":
-            df = df[2:]
-            df.columns = const.daily_columns_a1
+        # dayとmodeをscrapeから変えられるようにしておく
+        if "day" in kwargs.keys():
+            self.day = kwargs["day"]
+        if "mode" in kwargs.keys():
+            self.mode = kwargs["mode"]
+        self._update_base_url(self)
+
+        # 天気があるblock_noを取得できているならば、あとはdfを取得するだけ.
+        if self.block_no:
+            df = self._extract_df_from_html(self)
+
         else:
-            df = df[3:]
-            df.columns = const.daily_columns_s1
+            # その県の大きい観測所のhtml attributeを取得
+            points = self.search_observatory()
+
+            # なぜかpointsが2つずつ被りがあるので、block_noを取得して一位にする
+            block_no_list = self._get_block_no(self, points)
+
+            # 複数あるblock_noのうち、天気が取れているblockを採用してdfに格納する
+            for block_no in block_no_list:
+                self.block_no = block_no
+                df = self._extract_df_from_html(self)
+
+                # 天気の列に値があればbreakでfor文終了
+                if not df["weather_noon"].isnull().all():
+                    break
 
         return df
 
@@ -169,12 +165,32 @@ def zip2geo(postal):
     return res
 
 
+def zip2weather(postcode):
+    get_data = zip2geo(postcode)
+
+    jma_scraper = JmaScraper(get_data["prefecture"], year=2018, month=10, day=10, mode='daily')
+    daily_df = jma_scraper.scrape(mode='daily')
+
+    hour_allday_df = pd.DataFrame()
+    # 1日ごとに一行化と連結処理を行っていく
+    for day in range(1, len(daily_df)+1):
+        oneday_hour_series = pd.Series()
+        hourly_df = jma_scraper.scrape(mode='hourly', day=day).reset_index(drop=True)
+        hourly_df.set_index("hour", inplace=True)
+        for hour, row in hourly_df.iterrows():
+            row.index = [f"{col}_{hour}" for col in hourly_df.columns]
+            oneday_hour_series = pd.concat([oneday_hour_series, row])
+        # TODO 1日目のすべてのカラム、2日目のすべてのカラム、という順場になるように、カラムのsortをfalseにする
+        hour_allday_df = pd.concat([hour_allday_df, oneday_hour_series], axis=1)
+    hour_allday_df.columns = list(daily_df.index)
+    _ = hour_allday_df.T
+    master = pd.concat([daily_df, hour_allday_df.T], axis=1)
+    pass
+
+
 if __name__ == "__main__":
 
     postcode = "453-0061"
-    get_data = zip2geo(postcode)
-    jma_scraper = JmaScraper(get_data, year=2018, month=10, day=10, mode='daily')
-    df = jma_scraper.scrape()
-
+    zip2weather(postcode)
 
     pass
