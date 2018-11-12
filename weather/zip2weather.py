d4f8035675c541b522c8bb3a992d35ea6f51798a
Tomoya Koike <makeffort134@gmail.com>
2018-11-12 16:03:48 +0900

    can scrape

--- weather/zip2weather.py
+++ weather/zip2weather.py
@@ -1,18 +1,16 @@
-import os, time, re, math
-import requests
-import pandas as pd
-import sys
-from bs4 import BeautifulSoup
+import re
+import time
 from pathlib import Path
-import json
-
-import requests
-from bs4 import BeautifulSoup
 
 import const
+import pandas as pd
+import requests
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 
 def access_site(url):
+    # スクレイピング先のサーバーに負荷がかかりすぎないよう、0.5秒おく
     time.sleep(0.5)
     html = requests.get(url).content
     soup = BeautifulSoup(html, "lxml")
@@ -26,32 +24,51 @@ class JmaScraper:
         self.year = year
         self.month = month
         self.day = day
-        self.prec_no = prec_no # 県ごとにつけられる番号
-        self.block_no = block_no
-        self.mode = "daily"
-        # self.html, self.soup = access_site(self.base_url)
-        self._fill_prec_no(self)
+        self.prec_no = prec_no      # 県ごとにつけられる番号
+        self.block_no = block_no    # 観測点ごとにつけられる番号
+        self.mode = "daily"         # 日毎か時間毎か
+        self._fill_prec_no(self)    # 県名からprec_noを補完する
         self.pref_select_page_url = f"http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture.php?prec_no={self.prec_no}"
+        self._fill_block_no(self)   # block_noを補完する
+
 
     @staticmethod
-    def _update_base_url(self, unknown="s1"):
+    def _update_base_url(self) -> None:
+        """
+        self.modeやself.dayを変更したときに、アクセスすべきURLを更新する.
+        :return: None
+        """
         self.base_url = f"http://www.data.jma.go.jp/obd/stats/etrn/view/{self.mode}_s1.php?prec_no={self.prec_no}&block_no={self.block_no}&year={self.year}&month={self.month}&day={self.day}&view=p1"
 
     @staticmethod
-    def _fill_prec_no(self):
+    def _fill_prec_no(self) -> None:
         """
         __init__で与えられた県名で、prec_noを検索して代入する
-        :return:
+        :return: None
         """
         pref_code_df = self._load_pref_code()
         self.prec_no = pref_code_df[pref_code_df["pref"] == self.pref]["code"].values[0]
 
     @staticmethod
+    def _fill_block_no(self) -> None:
+        # その県の大きい観測所のhtml attributeを取得
+        points = self.search_observatory()
+
+        # なぜかpointsが2つずつ被りがあるので、block_noを取得して一位にする
+        block_no_list = self._get_block_no(self, points)
+
+        # 複数あるblock_noのうち、天気が取れているblockを採用してdfに格納する
+        for block_no in block_no_list:
+            self.block_no = block_no
+            break
+
+    @staticmethod
     def _load_pref_code():
         """
         気象庁の天気情報で県ごとに割り当てられているコードをcsvで保存し、保存されていれば読み込む。
-        :return: dataframeで、カラムに 県名 code を持つ
+        :return: dataframeで、カラムに 県名,code を持つ
         """
+
         if Path("pref_code.csv").exists():
             return pd.read_csv("pref_code.csv")
 
@@ -90,6 +107,10 @@ class JmaScraper:
             if "onmouseover" not in point.attrs.keys():
                 continue
 
+            # 天気に関する情報が少ない場合a, 多い場合sとするルールがあるため、aの観測所は除く
+            if point["onmouseover"].split(",")[0][-2] == 'a':
+                continue
+
             if point["onmouseover"].split(",")[13][1:2] == '1':
                 big_observatories.append(point)
 
@@ -125,25 +146,10 @@ class JmaScraper:
             self.mode = kwargs["mode"]
         self._update_base_url(self)
 
-        # 天気があるblock_noを取得できているならば、あとはdfを取得するだけ.
-        if self.block_no:
-            df = self._extract_df_from_html(self)
+        df = self._extract_df_from_html(self)
 
-        else:
-            # その県の大きい観測所のhtml attributeを取得
-            points = self.search_observatory()
-
-            # なぜかpointsが2つずつ被りがあるので、block_noを取得して一位にする
-            block_no_list = self._get_block_no(self, points)
-
-            # 複数あるblock_noのうち、天気が取れているblockを採用してdfに格納する
-            for block_no in block_no_list:
-                self.block_no = block_no
-                df = self._extract_df_from_html(self)
-
-                # 天気の列に値があればbreakでfor文終了
-                if not df["weather_noon"].isnull().all():
-                    break
+        # スクレイピングが正常に行えていないときにストップするため.
+        assert not df.empty
 
         return df
 
@@ -165,32 +171,58 @@ def zip2geo(postal):
     return res
 
 
-def zip2weather(postcode):
+def zip2weather(postcode, sdate, edate, mode='daily', duration=0):
     get_data = zip2geo(postcode)
 
-    jma_scraper = JmaScraper(get_data["prefecture"], year=2018, month=10, day=10, mode='daily')
-    daily_df = jma_scraper.scrape(mode='daily')
+    # 日毎では、日付の指定がありdurationを考慮する必要がない.
+    if mode == 'daily':
+        daily_columns = ["temperature_mean", "temperature_max", "temperature_min", "humidity_mean", "weather_noon",
+                         "weather_night"]
+        start_daily_df = JmaScraper(get_data["prefecture"], year=sdate.year, month=sdate.month, day=sdate.day).scrape()
+        end_daily_df = JmaScraper(get_data["prefecture"], year=edate.year, month=edate.month, day=edate.day).scrape()
 
-    hour_allday_df = pd.DataFrame()
-    # 1日ごとに一行化と連結処理を行っていく
-    for day in range(1, len(daily_df)+1):
-        oneday_hour_series = pd.Series()
-        hourly_df = jma_scraper.scrape(mode='hourly', day=day).reset_index(drop=True)
-        hourly_df.set_index("hour", inplace=True)
-        for hour, row in hourly_df.iterrows():
-            row.index = [f"{col}_{hour}" for col in hourly_df.columns]
-            oneday_hour_series = pd.concat([oneday_hour_series, row])
-        # TODO 1日目のすべてのカラム、2日目のすべてのカラム、という順場になるように、カラムのsortをfalseにする
-        hour_allday_df = pd.concat([hour_allday_df, oneday_hour_series], axis=1)
-    hour_allday_df.columns = list(daily_df.index)
-    _ = hour_allday_df.T
-    master = pd.concat([daily_df, hour_allday_df.T], axis=1)
-    pass
+        # 該当する日付の行を抜き出し、カラム名を変更する
+        start_day_series = start_daily_df[start_daily_df["day"] == str(sdate.day)][daily_columns]
+        start_day_series.columns = [f"s_{col}" for col in start_day_series.columns]
+        end_day_series = end_daily_df[end_daily_df["day"] == str(edate.day)][daily_columns]
+        end_day_series.columns = [f"e_{col}" for col in end_day_series.columns]
+
+        # 連結して返す
+        daily_series = pd.concat([start_day_series.reset_index(drop=True), end_day_series.reset_index(drop=True)], axis=1)
+        return daily_series
+
+    # dailyでないとき、つまりhourlyのとき
+    hourly_df_list = []
+    for date in [sdate, edate]:
+        jma_scraper = JmaScraper(get_data["prefecture"], year=date.year, month=date.month, day=date.day)
+        hourly_df = pd.DataFrame()
+
+        for i in range(duration*-1, duration+1):
+            changed_date = date + timedelta(days=i)
+            oneday_df = jma_scraper.scrape(mode='hourly', day=changed_date.day).reset_index(drop=True)
+            hourly_df = pd.concat([hourly_df, oneday_df], axis=0)
+
+        hourly_df_list.append(hourly_df)
+
+    return hourly_df_list
 
 
 if __name__ == "__main__":
 
-    postcode = "453-0061"
-    zip2weather(postcode)
+    source_df = pd.read_excel("zip20181105.xlsx")
+    aggregated_df = pd.DataFrame()
 
+    # 各データごとに、開始日の天気と終了日の天気をスクレイピングする
+    for i, row in source_df.iterrows():
+        save_folder = Path("weather_data") / row["folder"]
+        save_folder.mkdir(exist_ok=True)
+        pref = row["prefecture"]
+        start_df, end_df = zip2weather(int(row["zip"]), row["sday"], row["eday"], mode='hourly', duration=1)
+        start_df.to_csv(save_folder / "start.csv", index=False)
+        end_df.to_csv(save_folder / "end.csv", index=False)
+
+        daily_df = zip2weather(int(row["zip"]), row["sday"], row["eday"], mode='daily')
+        aggregated_df = pd.concat([aggregated_df, daily_df], axis=0)
+
+    aggregated_df = pd.concat([source_df, aggregated_df.reset_index(drop=True)], axis=1)
     pass
