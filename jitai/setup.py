40704db28545e9c88d9b51cdfb6ab639f1761878
Tomoya Koike <makeffort134@gmail.com>
2018-12-28 18:08:41 +0900

    setup.py modified due to server data sync bug

--- jitai/setup.py
+++ jitai/setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name='jitai',
     version='0.0.1',
     description='python program for Just-in-time adaptive intervention',
+    long_description=readme,
     author='Tomoya Koike',
     author_email='makeffort134@gmail.com',
     url='https://github.com/Tomoya-K-0504/research/tree/master/jitai',
     license=license,
-    packages=['jitai/src', 'jitai/config']
+    packages=find_packages(exclude=('tests', 'docs'))
 )
