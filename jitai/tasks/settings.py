015e7b2644b764a5ca900a6823ded2223b72d0c5
Tomoya Koike <makeffort134@gmail.com>
2019-01-20 14:07:52 +0900

    pipeline base code

--- jitai/tasks/settings.py
+++ jitai/tasks/settings.py
@@ -1,6 +1,8 @@
 import yaml
 from datetime import datetime, timedelta
 from pathlib import Path
+from abc import ABC
+from abc import abstractmethod
 
 from jitai.config import const
 from jitai.config import logger as logger_file
@@ -16,6 +18,7 @@ CLASS_CONVERTER = {"mother_wake": Wake, "mother_sleep": Sleep}
 
 """
 
+condition_name: sth
 event: ["sleep", "wake", "nap", "scheduled", "default"]
 ema_time: ["set_time", "interval"]
 ema_time_values: [["from", "to"], ""]
@@ -25,11 +28,13 @@ intervene_time: {"set_time", "interval"}
 
 # No.3の介入
 params = yaml.load("""
-- event: mother_sleep
+- condition_name: sleep_before_evening
+  event: mother_sleep
   ema_time: 
     set_time:
         from: "19:00"
-- event: mother_wake
+- condition_name: wake_early_morning
+  event: mother_wake
   ema_time: 
     set_time:
         from: "2:00"
@@ -39,6 +44,13 @@ params = yaml.load("""
 
 
 class Pipeline:
+    """
+    パイプラインが満たすべき条件は以下に示す
+    - stepsによって処理を進める.
+    - 最終ステップ(介入)以外の各stepで, EMAや実行時刻等に条件を適用し、条件を満たせばTrueで次ステップへ処理済みEMAを渡す
+    - 各ステップで条件を満たさない場合は、次ステップにFalseを渡し、
+
+    """
     def __init__(self, steps):
         self.steps = steps
 
@@ -46,8 +58,20 @@ class Pipeline:
         pass
 
 
+class Step(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        pass
+
+
 if __name__ == "__main__":
-    pipeline = Pipeline()
+    steps = []
     for param in params:
         Jitai = CLASS_CONVERTER[param["event"]]
-        jitai = Jitai()
+        jitai = Jitai(param)
+        steps.append((params["condition_name"], jitai))
+    pipeline = Pipeline(steps)
+    pipeline()
