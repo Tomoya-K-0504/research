770cddfb75a9e460c31d36db37348cdd50a23873
Tomoya Koike <makeffort134@gmail.com>
2019-01-26 16:46:26 +0900

    fetch EMA done

---
+++ jitai/tasks/fetch_ema.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pandas as pd
+from jitai.config import const
+from jitai.config import logger as logger_file
+from jitai.src.EmaRecorder import EmaRecorder
+from jitai.src.User import User
+
+
+if __name__ == "__main__":
+
+    logger = logger_file.logger(const.LOG_DIR)
+
+    for index, user_info in const.USER_LIST.iterrows():
+
+        user_data_dir = const.DATA_DIR / user_info["terminal_id"]
+
+        # 前回EMAを取得した時間を取得する.
+        if Path(user_data_dir / "ema_history.txt").exists():
+            with open(user_data_dir / "ema_history.txt", "r") as f:
+                last_ema_time = f.readlines()[-1]
+        else:
+            last_ema_time = ""
+
+        # 今回のEMAを記録する.
+        with open(user_data_dir / "ema_history.txt", "a") as f:
+            f.write("\n" + datetime.today().strftime("%Y%m%d%H%M%S"))
+
+        ema_recorder = EmaRecorder(logger, User(user_info["terminal_id"]))
+        ema, _, _ = ema_recorder(from_date=last_ema_time, to_date="")
+
+        if not ema.empty:
+
+            if Path(user_data_dir / "ema.csv").exists():
+                prev_ema = pd.read_csv(user_data_dir / "ema.csv")
+                ema = pd.concat([prev_ema, ema], axis=0)
+
+            ema.to_csv(user_data_dir / "ema.csv")
+
+            logger.info("EMA of {} updated .".format(user_info["terminal_id"]))
